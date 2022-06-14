ef838d457bbf816239eeea8fc1688372fd5727bb
mihail <demini2@mail.ru>
2022-06-14 18:16:48 +0300

    ну да

---
+++ admin/view/View.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace view;
+
+class View
+{
+    private array $arrayContent = [];
+
+    public function __set(string $name, array $values): void
+    {
+        $this->arrayContent[$name] = $values;
+    }
+
+    public function __get(string $name): array|null
+    {
+        return $this->arrayContent[$name] ?? null;
+    }
+
+    public function __isset(string $name): bool
+    {
+        return isset($this->arrayContent[$name]);
+    }
+
+    private function render(string $template): string
+    {
+        $wey = __DIR__ . '/../templates/' . $template;
+        ob_clean();
+        include $wey;
+        $content = ob_get_contents();
+        ob_end_clean();
+        return $content;
+    }
+
+    public function display(string $template): void
+    {
+        echo $this->render($template);
+    }
+}
